5f7515fb7400eecbfeab410a6e1031591d20a184
Victor Edward Calle <victor@bybsoft.com>
2019-04-29 19:59:06 -0600

    Including logic for:
    As admin: add / edit / delete product
    
    User: check product details, add to cart, list cart content.
    
    Listing products for user and admin also were improved.

---
+++ models/cart.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+const p = path.join(rootDir, 'data', 'cart.json');
+
+module.exports = class Cart {
+    static addProduct(id, price){
+        // Fetch the previous cart
+        fs.readFile(p, (err, fileContent) => {
+            let cart = {products: [], totalPrice: 0};
+            if(!err){
+                cart = JSON.parse(fileContent);
+            }
+
+            // analyze cart => find existing product
+            const existingProductIndex = cart.products.findIndex(product => product.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+            // add new product / increase quantity
+            if(existingProduct){
+                updatedProduct = {...existingProduct};
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                updatedProduct = { id: id, qty: 1 };
+                cart.products = [...cart.products, updatedProduct];
+            }
+            cart.totalPrice = cart.totalPrice + +price;
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            });
+        });
+    }
+
+    static deleteProduct(id, productPrice){
+        fs.readFile(p, (err, fileContent) => {
+            if(err){
+                return;
+            }
+
+            let cart = JSON.parse(fileContent);
+            const updatedCart = {...cart};
+            const product = updatedCart.products.find(prod => prod.id === id);
+
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+            updatedCart.totalPrice = updatedCart.totalPrice - (productPrice * productQty);
+
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
+                console.log(err);
+            });
+        });
+    }
+
+    static getProducts(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+
+            if(err)
+                cb(null);
+            else
+                cb(cart);
+        });
+    }
+};
