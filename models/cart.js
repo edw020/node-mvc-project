da45ae568b88f436d6cb4b371aa71e7a6acaed8a
Victor Edward Calle <victor@bybsoft.com>
2019-04-30 19:17:02 -0600

    Including functionality for removing items from cart.

--- models/cart.js
+++ models/cart.js
@@ -45,13 +45,15 @@ module.exports = class Cart {
             const updatedCart = {...cart};
             const product = updatedCart.products.find(prod => prod.id === id);
 
-            const productQty = product.qty;
-            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
-            updatedCart.totalPrice = updatedCart.totalPrice - (productPrice * productQty);
+            if(product){
+                const productQty = product.qty;
+                updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+                updatedCart.totalPrice = updatedCart.totalPrice - (productPrice * productQty);
 
-            fs.writeFile(p, JSON.stringify(updatedCart), err => {
-                console.log(err);
-            });
+                fs.writeFile(p, JSON.stringify(updatedCart), err => {
+                    console.log(err);
+                });
+            }
         });
     }
 
